b641cb32461f69ab7c20842a257832ca2e7e045e
Alexander Heinlein <alexander.heinlein@web.de>
2013-07-18 18:57:03 +0200

    showPerUserMem.sh: Select user directly via ps argument
    This works better for newer ps versions where long user names are truncated
    to 8 chars and seems cleaner.

--- showPerUserMem.sh
+++ showPerUserMem.sh
@@ -11,5 +11,5 @@ TOTAL=$(free | awk '/Mem:/ { print $2 }')
 
 for USER in $(who | awk '{print $1}' | sort -u)
 do
-    ps haux | awk -v user=$USER -v total=$TOTAL '$1 ~ user { sum += $6} END { printf "%s %.2f\n", user, sum / total * 100; }'
+    ps hux -U $USER | awk -v user=$USER -v total=$TOTAL '{ sum += $6} END { printf "%s %.2f\n", user, sum / total * 100; }'
 done
