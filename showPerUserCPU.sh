183b8b462540bc5b01b3386a8a105d47434e6f7c
Alexander Heinlein <alexander.heinlein@web.de>
2018-08-08 08:55:28 +0200

    Show CPU usage ratio
    Based on edit by Shabirmean (https://unix.stackexchange.com/posts/120581/revisions)

--- showPerUserCPU.sh
+++ showPerUserCPU.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# print total CPU usage in percent of currently logged in users
+# print total CPU usage in percent of currently logged in users.
 #
 # to sort by CPU usage, pipe the output to 'sort -k2 -nr'
 #
@@ -8,17 +8,18 @@
 set -e
 
 own=$(id -nu)
+cpus=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
 
 for user in $(who | awk '{print $1}' | sort -u)
 do
     # print other user's CPU usage in parallel but skip own one because
     # spawning many processes will increase our CPU usage significantly
     if [ "$user" = "$own" ]; then continue; fi
-    (top -b -n 1 -u "$user" | awk -v user=$user 'NR>7 { sum += $9; } END { print user, sum; }') &
+    (top -b -n 1 -u "$user" | awk -v user=$user -v CPUS=$cpus 'NR>7 { sum += $9; } END { print user, sum, sum/CPUS; }') &
     # don't spawn too many processes in parallel
     sleep 0.05
 done
 wait
 
 # print own CPU usage after all spawned processes completed
-top -b -n 1 -u "$own" | awk -v user=$own 'NR>7 { sum += $9; } END { print user, sum; }'
+top -b -n 1 -u "$own" | awk -v user=$own -v CPUS=$cpus 'NR>7 { sum += $9; } END { print user, sum, sum/CPUS; }'
