17250bee1730746ea345ef813b4d3352a3eb4555
Alexander Heinlein <alexander.heinlein@web.de>
2020-04-19 17:33:52 +0200

    Add backup script

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 script snippets
 ===============
 
-handy snippets of various scripts, including:
+Handy snippets of various scripts, including:
 
-* `diffArchive.sh`: shows the diff of two archive contents
-* `listLargeDebPackages.sh`: list all installed debian packages sorted by their size
-* `listLargeDirs.sh`: recursively list all directories sorted by their content size
-* `listLargeFiles.sh`: recursively list all files sorted by their size
-* `osd_mpd.sh`: small 'daemon' to show the currently playing mpd song as on-screen display
-* `showAllPerUserCPU.sh`: print the *total* CPU usage in percent of *all* available users
-* `showPerUserCPU.sh`: print the *total* CPU usage in percent of each user logged in
-* `showPerUserMem.sh`: print the *total* memory usage in percent of each user logged in
+* `backup.sh`: creates user and (full) system backups. make sure to check the list of excludes beforehand.
+* `diffArchive.sh`: shows the diff of two archive contents.
+* `listLargeDebPackages.sh`: list all installed debian packages sorted by their size.
+* `listLargeDirs.sh`: recursively list all directories sorted by their content size.
+* `listLargeFiles.sh`: recursively list all files sorted by their size.
+* `osd_mpd.sh`: small 'daemon' to show the currently playing mpd song as on-screen display.
+* `showAllPerUserCPU.sh`: print the *total* CPU usage in percent of *all* available users.
+* `showPerUserCPU.sh`: print the *total* CPU usage in percent of each user logged in.
+* `showPerUserMem.sh`: print the *total* memory usage in percent of each user logged in.
 
 License
 -------
 [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/)
-(c) [Alexander Heinlein](http://choerbaert.org)
+(c) [Alexander Heinlein](mailto:alexander.heinlein@web.de)
---
+++ backup.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+#
+# creates user and system backup.
+# pass option -f to create a full system backup instead of a compact one.
+#
+
+basedir=$(dirname "$0")
+
+# exclude files and directories listed in these files,
+# user-specific excludes:
+excl_home="$basedir/backup_excludes_home.txt"
+# system-specific excludes:
+excl_sys="$basedir/backup_excludes_sys.txt"
+
+# check for exclude files
+if ! [ -f "$excl_home" ]; then
+    echo "exclude file \"$excl_home\" is missing!"
+    exit 1
+fi
+if ! [ -f "$excl_sys" ]; then
+    echo "exclude file \"$excl_sys\" is missing!"
+    exit 1
+fi
+
+# check for root
+if ! [ $(id -u) = 0 ]; then
+    echo "warning: you should be root to run this script."
+    echo
+    sleep 5
+fi
+
+# check options
+if [ "$1" = "-f" ]; then
+    full_backup="1"
+fi
+
+# tar options and extension
+#  -c create
+#  -p preserve permissions
+#  -j bzip2 compression
+#  -J xz compression
+#  -z gzip compression
+#  -v verbose output
+#  -f filename
+tar_opts="cpJvf"
+tar_ext="tar.xz"
+
+# enable parallel compression for xz
+export XZ_DEFAULTS="-T 0"
+
+# create backup
+date="$(date +%Y-%m-%d)"
+file_base="backup-$(hostname)-$date"
+file_home="${file_base}-home.$tar_ext"
+echo "creating user backup $file_home"
+echo time tar $tar_opts "$file_home" --exclude-from="$excl_home" /home/ /root/ 2>"${file_base}-home.log"
+echo
+if ! [ $full_backup ]; then
+    file_sys="${file_base}-sys.$tar_ext"
+    echo "creating system backup $file_sys"
+    echo time tar $tar_opts "$file_sys" --exclude-from="$excl_sys" /etc/ /var/spool/ /var/www/ 2>"${file_base}-sys.log"
+else
+    file_sys="${file_base}-sys-full.$tar_ext"
+    echo "creating full system backup $file_sys"
+    echo time tar $tar_opts "$file_sys" --exclude-from="$excl_sys" / 2>"${file_base}-sys.log"
+fi
+
+echo
+echo "done."
